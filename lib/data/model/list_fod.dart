a0b585aba6daca056e2ba8dab03aab412a3ba308
MinhNhat <fatdinasours@gmail.com>
2023-08-25 11:32:33 +0700

    nmn update

---
+++ lib/data/model/list_fod.dart
@@ -0,0 +1,27 @@
+class listFood {
+  String? name;
+  String? avatar;
+  String? desscriptions;
+  int? time;
+  String? id;
+
+  listFood({this.name, this.avatar, this.desscriptions, this.time, this.id});
+
+  listFood.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    avatar = json['avatar'];
+    desscriptions = json['desscriptions'];
+    time = json['time'];
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['name'] = name;
+    data['avatar'] = avatar;
+    data['desscriptions'] = desscriptions;
+    data['time'] = time;
+    data['id'] = id;
+    return data;
+  }
+}
