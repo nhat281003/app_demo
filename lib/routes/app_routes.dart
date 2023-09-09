ece450e43b6a40042d90f41de725a232991acd03
MinhNhat <fatdinasours@gmail.com>
2023-09-09 16:12:13 +0700

    nmn update

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,6 +2,7 @@ class Routes{
   static const home = '/home';
   static const spinLucky = '/spinLucky';
   static const login = '/login';
+  static const test = '/test';
   static const splash = '/splash';
   static const splash2 = '/splash2';
   static const order = '/order';
