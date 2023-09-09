3985428a97d7a72501089d2f09ec8103531ad891
MinhNhat <fatdinasours@gmail.com>
2023-09-09 16:17:30 +0700

    nmn update

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -10,6 +10,7 @@ import 'package:get/get.dart';
 
 class AppPages{
   static final pages = [
+
     GetPage(name:Routes.splash, page: ()=> SplashScreen()),
     GetPage(name:Routes.home, page: ()=> HomeScreen()),
     GetPage(name:Routes.login, page: ()=> LoginScreen()),
